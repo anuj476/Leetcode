ecdadc710b1e9d7d3f5f52f42b50d6971e9c73e1
ANUJ RANJAN <singhmohit4422@gmail.com>
2026-09-02 13:18:37 +0530

    Time: 9 ms (46.35%), Space: 46.6 MB (95.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,21 @@
-class Solution {
+    class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[][] arr = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = arr[left][0] + arr[right][0];
+            if (sum == target) {
+                return new int[] { arr[left][1], arr[right][1] };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         return new int[] {};
